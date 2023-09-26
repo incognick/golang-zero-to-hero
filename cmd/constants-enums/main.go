3d735e14605072da701ed1bb4d1594b860da2197
Nicholas Phillips <nsphilli@gmail.com>
2023-09-26 16:28:26 -0500

    adds constants and enums lecture

---
+++ cmd/constants-enums/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type State int
+
+const (
+	StatePrep State = iota + 1
+	StateCooking
+	StateDone
+)
+
+type Color int
+
+const (
+	ColorGreen Color = iota
+	ColorBlue
+	ColorRed
+)
+
+func main() {
+	fmt.Println("constants and enums")
+
+	handleState(StatePrep)
+	handleState(ColorGreen)
+}
+
+func handleState(state State) {
+	fmt.Println(state)
+}
